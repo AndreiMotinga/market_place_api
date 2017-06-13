66857afd6d879994b243979d202cb07e44960299
Andrei Motinga <andrew.motinga@gmail.com>
2017-06-13 18:05:11 -0400

    Adds user order has many relation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,8 +6,8 @@ describe User do
   it { should allow_value('example@domain.com').for(:email) }
 
   it { should validate_uniqueness_of(:auth_token)}
-
   it { should have_many(:products) }
+  it { should have_many(:orders) }
 
   describe '#generate_authentication_token!' do
     before { @user = create :user }
