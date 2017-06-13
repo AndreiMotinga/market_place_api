fe24e89b78ff7b1a27b76202afbe102aae5c8d80
Andrei Motinga <andrew.motinga@gmail.com>
2017-06-13 13:07:06 -0400

    Adds authorization for the users controller

--- spec/controllers/concerns/authenticable_spec.rb
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 class Authentication
   include Authenticable
@@ -17,6 +17,23 @@ describe Authenticable do
 
     it 'returns the user from the authorization header' do
       expect(authentication.current_user.auth_token).to eql @user.auth_token
+    end
+  end
+
+  describe '#authenticate_with_token' do
+    before do
+      @user = create :user
+      allow(authentication).to receive(:current_user).and_return(nil)
+      allow(response).to receive(:response_code).and_return(401)
+      allow(response)
+        .to receive(:body)
+        .and_return({ errors: 'Not authenticated' }.to_json)
+    end
+
+    it { should respond_with 401 }
+
+    it 'render a json error message' do
+      expect(json_response[:errors]).to eql 'Not authenticated'
     end
   end
 end
