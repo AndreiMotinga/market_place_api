76eb9c9df0d6f1f88d854601360041395121362c
Andrei Motinga <andrew.motinga@gmail.com>
2017-06-13 11:26:04 -0400

    Adds the user create endpoint

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -16,4 +16,40 @@ describe Api::V1::UsersController do
 
     it { should respond_with 200 }
   end
+
+  describe 'POST #create' do
+    context 'when is successfully created' do
+      before do
+        @user_attributes = attributes_for :user
+        post :create, params: { user: @user_attributes }
+      end
+
+      it { should respond_with 201 }
+
+      it 'renders the json representation for the user record just created' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql @user_attributes[:email]
+      end
+    end
+
+    context 'when is not created' do
+      before do
+        invalid_user_attributes = { password: '12345678',
+                                    password_confirmation: '12345678' }
+        post :create, params: { user: invalid_user_attributes }
+      end
+
+      it { should respond_with 422 }
+
+      it 'renders an errors json' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+      it 'renders the json errors on why the user could not be created' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include "can't be blank"
+      end
+    end
+  end
 end
