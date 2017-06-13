34f01989f17dbcad03ef70836666d8b567201c69
Andrei Motinga <andrew.motinga@gmail.com>
2017-06-13 12:02:57 -0400

    Refactors the json parse method

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -10,8 +10,7 @@ describe Api::V1::UsersController do
     end
 
     it 'returns the information about a reporter on a hash' do
-      user_response = JSON.parse(response.body, symbolize_names: true)
-      expect(user_response[:email]).to eql @user.email
+      expect(json_response[:email]).to eql @user.email
     end
 
     it { should respond_with 200 }
@@ -27,8 +26,7 @@ describe Api::V1::UsersController do
       it { should respond_with 201 }
 
       it 'renders the json representation for the user record just created' do
-        user_response = JSON.parse(response.body, symbolize_names: true)
-        expect(user_response[:email]).to eql @user_attributes[:email]
+        expect(json_response[:email]).to eql @user_attributes[:email]
       end
     end
 
@@ -42,13 +40,11 @@ describe Api::V1::UsersController do
       it { should respond_with 422 }
 
       it 'renders an errors json' do
-        user_response = JSON.parse(response.body, symbolize_names: true)
-        expect(user_response).to have_key(:errors)
+        expect(json_response).to have_key(:errors)
       end
 
       it 'renders the json errors on why the user could not be created' do
-        user_response = JSON.parse(response.body, symbolize_names: true)
-        expect(user_response[:errors][:email]).to include "can't be blank"
+        expect(json_response[:errors][:email]).to include "can't be blank"
       end
     end
   end
@@ -64,8 +60,7 @@ describe Api::V1::UsersController do
       it { should respond_with 200 }
 
       it 'renders the json representation for the updated user' do
-        user_response = JSON.parse(response.body, symbolize_names: true)
-        expect(user_response[:email]).to eql 'newmail@example.com'
+        expect(json_response[:email]).to eql 'newmail@example.com'
       end
     end
 
@@ -84,8 +79,7 @@ describe Api::V1::UsersController do
       end
 
       it 'renders the json errors on whye the user could not be created' do
-        user_response = JSON.parse(response.body, symbolize_names: true)
-        expect(user_response[:errors][:email]).to include 'is invalid'
+        expect(json_response[:errors][:email]).to include 'is invalid'
       end
     end
   end
